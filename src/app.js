3d5d6771966c89aa81ad4201b33307b7eaafab97
Morgan Powers <morganpowers@Morgans-MacBook-Pro-3.local>
2021-06-07 22:00:16 -0400

    module 4 attempt 1

--- src/app.js
+++ src/app.js
@@ -8,6 +8,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded(extended=true));
 
 const accountData = fs.readFileSync(
   path.join(__dirname, 'json', 'accounts.json'), 'utf-8'
