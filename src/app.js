f5dc42784b48d5334c15c896972770bae0d18063
Morgan Powers <morganpowers@Morgans-MacBook-Pro-3.local>
2021-06-07 22:48:42 -0400

    module 6 attempt 4

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const accountRoutes = require('./routes/accounts.js');
-const serviceRoutes = require('./routes/services.js');
+const servicesRoutes = require('./routes/services.js');
 
 const app = new express();
 
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded(extended=true));
 
 app.use('/account', accountRoutes);
-app.use('/services', serviceRoutes);
+app.use('/services', servicesRoutes);
 
 app.get('/', (req, res) => 
   res.render('index', { title: 'Account Summary', accounts
