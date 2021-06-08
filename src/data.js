b10faedd79617def7e31ca2c99d6dcb2ba263421
Morgan Powers <morganpowers@Morgans-MacBook-Pro-3.local>
2021-06-07 22:28:34 -0400

    module 5 attempt 3

--- src/data.js
+++ src/data.js
@@ -11,7 +11,7 @@ const userData = fs.readFileSync(
 );
 const users = JSON.parse(userData);
 
-writeJSON => {
+const writeJSON = () => {
   const accountsJSON = JSON.stringify(accounts, null, 4);
   fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
 };
